9b06e03bf0748a323ca9631e030ddff0cd58fefe
Arman <avalyanarman@gmail.com>
2021-05-16 16:29:49 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Store store = new Store(15,"Arthur",12500678,12);
+        System.out.println(store);
+        store.selling();
+        System.out.println(store);
+        store.selling();
+        System.out.println(store);
+    }
+}
---
+++ src/com/company/Store.java
@@ -0,0 +1,64 @@
+package com.company;
+
+public class Store {
+
+    private int countOfWorkers;
+    private String name;
+    private int phoneNumber;
+    private int productNumber;
+
+
+    public Store(int countOfWorkers, String name, int phoneNumber, int productNumber){
+        setCountOfWorkers(countOfWorkers);
+        setName(name);
+        setPhoneNumber(phoneNumber);
+        setProductNumber(productNumber);
+    }
+
+    @Override
+    public String toString() {
+        return  "Count of workers - " + countOfWorkers + "\n" +
+                "Name - " + name + "\n" +
+                "Phone number - " + phoneNumber  + "\n" +
+                "Product number - " + productNumber  + "\n";
+    }
+
+    public void selling() {
+        setProductNumber(getProductNumber() - 1);
+    }
+
+    public int getCountOfWorkers() {
+        return countOfWorkers;
+    }
+
+    public void setCountOfWorkers(int countOfWorkers) {
+        if (countOfWorkers >= 2 && countOfWorkers <= 50)
+            this.countOfWorkers = countOfWorkers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.length() >= 3)
+            this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        if (String.valueOf(phoneNumber).length() == 8)
+            this.phoneNumber = phoneNumber;
+    }
+
+    public int getProductNumber() {
+        return productNumber;
+    }
+
+    public void setProductNumber(int productNumber) {
+        this.productNumber = productNumber;
+    }
+}
